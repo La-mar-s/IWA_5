e7f4562b87d781706833245ee6b4a58ac3233834
La-mar-s <sony.lamar.1995@gmail.com>
2023-06-28 00:43:33 +0200

    modified challenge_1

--- Challenge_1/scripts.js
+++ Challenge_1/scripts.js
@@ -1,35 +1,51 @@
-FREE_WARNING = 'Free shipping only applies to single customer orders';
-BANNED_WARNIN = 'Unfortunately we do not ship to your country of residence';
-NONE_SELECTED = '0';
+const FREE_WARNING = "Free shipping only applies to single customer orders";
+const BANNED_WARNING =
+  "Unfortunately we do not ship to your country of residence";
+const NONE_SELECTED = 0;
 
-if (location = RSA) { shipping === 400 && currency === 'R' }
+let shoes = 300 * 1;
+let toys = 100 * 5;
+let shirts = 150 * NONE_SELECTED;
+let batteries = 35 * 2;
+let pens = 5 * NONE_SELECTED;
 
-if (location = NAM) {
-shipping = 600 
+let shippingItems = shoes + toys + shirts + batteries + pens;
+
+let shipping;
+let currency;
+
+let customers = "1";
+let country = "RSA";
+
+if (country === "RSA") {
+  shipping = 400;
+  currency = "R";
+} else if (country === "NAM") {
+  shipping = 600;
+  currency = "$";
 } else {
-    shipping = 800
-}
-shoes = 300 * 1
-toys = 100 * 5
-shirts = 150 * 'NONE_SELECTED'
-batteries = 35 * 2
-pens = 5 * 'NONE_SELECTED' 
-
-let shipping = null
-let currency = $
-
-if (shoes + batteries + pens + shirts > 1000 && toys ) {
-	if (location = NAM && customers < 2) {
-			if (location = RSA)
-		    shipping = 0 || calcShipping
-		}
+  shipping = 800;
+  currency = "$";
 }
 
+//clue no.1 : >=
+//clue no.2 : amount R1000 $60
+//clue no.3 : location RSA NAM
+//clue no.4 : shipping free if RSA or NAM and price
+//clue no.5 : customers = 1
+//clue no.6 : console warning if more than 1 customer
 
-if (shipping = 0  && customers !== 1) { console.log(WARNING) }
+if (
+  (shippingItems >= 1000 && country === "RSA") ||
+  (shippingItems >= 60 && country === "NAM")
+) {
+  if (customers === "1") {
+    shipping = 0;
+  } else {
+    console.log(FREE_WARNING);
+  }
+}
 
-location = 'NK' ? console.log(WARNING) : console.log('price', currency, shoes + batteries + pens + shirts + shipping)
-
-let customers = '1'
-let location = 'RSA'
-currency = null
+country === "NK"
+  ? console.log(BANNED_WARNING)
+  : console.log("Price:", currency, shippingItems + shipping);
